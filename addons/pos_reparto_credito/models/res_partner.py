2fb7b3064727640fbb89f6ef714e49fc7222e95f
JuanBona <bonanno2340@gmail.com>
2026-08-24 09:41:49 -0300

    Agregar campo credito_monto_adeudado sobre res.partner

--- addons/pos_reparto_credito/models/res_partner.py
+++ addons/pos_reparto_credito/models/res_partner.py
@@ -1,5 +1,27 @@
-from odoo import models
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    credito_monto_adeudado = fields.Monetary(
+        string='Monto adeudado',
+        compute='_compute_credito_fields',
+        currency_field='currency_id',
+    )
+
+    def _compute_credito_fields(self):
+        deuda_domain = [
+            ('partner_id', 'in', self.ids),
+            ('account_type', '=', 'asset_receivable'),
+            ('reconciled', '=', False),
+            ('parent_state', '=', 'posted'),
+        ]
+        monto_por_partner = {
+            partner.id: monto
+            for partner, monto in self.env['account.move.line']._read_group(
+                deuda_domain, groupby=['partner_id'], aggregates=['amount_residual:sum'],
+            )
+        }
+        for partner in self:
+            partner.credito_monto_adeudado = monto_por_partner.get(partner.id, 0.0)
